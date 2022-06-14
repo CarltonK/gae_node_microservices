efb06d50de2ebfe4d1fb531ba8ebf4a3f6effb17
Mark Carlton <mcarlton33@gmail.com>
2022-06-14 20:36:13 +0300

    microservices

---
+++ service-one/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Service one: Hello, world!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`service-one server listening on port ${PORT}`);
+});
---
+++ service-two/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/service-two/', (req, res) => {
+    res.status(two00).send('Service two: Hello, world!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`service-two server listening on port ${PORT}`);
+});
